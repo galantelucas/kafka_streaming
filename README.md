099214fe1403469cca9cece7a579f4864a529ed9
WillFernandes <wilkerson-carvalho@outlook.com>
2024-09-23 23:23:44 -0300

    Adicionando alterações no producer e consumer, alterações no dashboard e visualizações, com georreferenciamento nos dados

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# kafka_streaming
+# Kafka Streaming Project
+
+Este projeto implementa um pipeline de dados em tempo real utilizando Apache Kafka. Abaixo está uma visão geral das principais componentes e funcionalidades do projeto.
+
+## Estrutura do Projeto
+
+### 1. Configuração do Kafka
+- Configuração necessária para iniciar e gerenciar um cluster Kafka.
+- Definição de tópicos, produtores e consumidores.
+### 2. Produção de Dados
+- Scripts ou aplicações que geram dados e os enviam para os tópicos Kafka.
+- Fontes de dados podem incluir logs de aplicativos, transações financeiras, etc.
+### 3. Processamento de Dados
+- Utilização da Kafka Streams API para processar dados em tempo real.
+- Operações incluem filtragem, agregação e transformação dos dados.
+### 4. Armazenamento de Dados
+- Dados processados são armazenados em um banco de dados ou enviados para outro sistema para análise posterior, utilizamos Postgres SQL para armazenamento.
+### 5. Monitoramento e Logs
+- Ferramentas para monitorar o fluxo de dados e registrar eventos importantes.
+- Garantia de funcionamento correto do sistema e facilitação da detecção de problemas.
+## Como Executar
+## Passo a Passo
+
+ **Clone o Repositório**:
+   ```bash
+   git clone https://github.com/galantelucas/kafka_streaming.git
+   cd kafka_streaming
+   Docker composse up -d
+   Verificar no localhost a aplicação http://localhost:8501/
+   ```
+
+O **Dashboard de Vendas** fornece uma visualização clara dos dados de vendas de um banco de dados PostgreSQL. Suas principais funcionalidades incluem:
+
+## Funcionalidades Principais
+
+1. **Conexão com o Banco de Dados**: 
+   - Busca dados de vendas, como ID, produto, valor, localização e data.
+
+2. **Visualização Georreferenciada**: 
+   - Exibe um mapa interativo mostrando onde as vendas ocorrem, facilitando a identificação de padrões geográficos.
+
+3. **Resumo de Vendas**: 
+   - Apresenta métricas chave, como total de vendas e número de transações, em cards.
+
+4. **Tabela de Vendas por Produto**: 
+   - Mostra detalhes de cada transação, permitindo análise granular.
+
+5. **Distribuição de Vendas por Produto**: 
+   - Um gráfico de rosca ilustra a participação de cada produto nas vendas totais.
+
+6. **Vendas ao Longo do Tempo**: 
+   - Um gráfico de linha mostra tendências de vendas mensais, ajudando a identificar sazonalidades.
+
+## Benefícios
+
+O dashboard facilita a tomada de decisões informadas, permitindo análises de desempenho e oportunidades de melhoria nas vendas.
+O incremento de visualizações e organizações poderá ser muito bem explorado pelo stakeholder.
+
+---
+
+![image](![Dashboard de Vendas em Streaming](image.png))
+
+
--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,9 +1,11 @@
 import psycopg2
 import pandas as pd
 import streamlit as st
-import time
+from decimal import Decimal
+import plotly.express as px
 
-# Função para buscar dados do banco de dados
+# Função para buscar dados do banco de dados, com cache de 5 segundos para atualizar periodicamente
+@st.cache_data(ttl=5)  # Atualiza os dados a cada 5 segundos
 def fetch_data():
     conn = psycopg2.connect(
         host="db",
@@ -12,17 +14,93 @@ def fetch_data():
         password="postgres"
     )
     cur = conn.cursor()
-    cur.execute('SELECT sale_id, product, amount FROM sales')
+    cur.execute('SELECT sale_id, product, amount, latitude, longitude, sale_date FROM sales')
     rows = cur.fetchall()
     cur.close()
     conn.close()
-    return pd.DataFrame(rows, columns=['sale_id', 'product', 'amount'])
+    
+    # Converte os dados em DataFrame e ajusta os tipos de dados
+    df = pd.DataFrame(rows, columns=['sale_id', 'product', 'amount', 'latitude', 'longitude', 'sale_date'])
+    df['amount'] = df['amount'].apply(lambda x: float(x) if isinstance(x, Decimal) else x)
+    df['latitude'] = df['latitude'].apply(lambda x: float(x) if isinstance(x, Decimal) else x)
+    df['longitude'] = df['longitude'].apply(lambda x: float(x) if isinstance(x, Decimal) else x)
+    df['sale_date'] = pd.to_datetime(df['sale_date'], format='%Y%m%d')
+    
+    return df
 
-st.title('Dados de Vendas')
+# Título do dashboard
+st.title('Dashboard de Vendas')
 
-# Exibindo dados periodicamente
-while True:
-    df = fetch_data()
-    st.dataframe(df)
-    time.sleep(5)  # Atualiza a cada 5 segundos
-    st.rerun()
+# Menu de navegação
+menu = st.sidebar.selectbox("Menu", ["Detalhamento de Produtos e Vendas", "Visualização Georreferenciada"])
+
+# Exibe os dados com base na seleção do menu
+df = fetch_data()  # Carrega os dados com cache e atualização automática
+
+if menu == "Visualização Georreferenciada":
+    st.markdown("### Mapa de Vendas por Localização")
+    st.map(df[['latitude', 'longitude']])
+    st.write("")  
+
+elif menu == "Detalhamento de Produtos e Vendas":
+    # Calcula total de vendas e número de transações
+    total_sales = df['amount'].sum()
+    num_transactions = len(df)
+    
+    # Layout das métricas com cards customizados
+    st.markdown("### Resumo de Vendas")
+    st.write("") 
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown(
+            f"""
+            <div style="background-color:#021035;padding:20px;border-radius:10px;text-align:center;margin-bottom:20px;">
+                <h3 style="color:#4baab4">Total de Vendas</h3>
+                <p style="font-size:24px;color:white">${total_sales:,.2f}</p>
+            </div>
+            """, unsafe_allow_html=True
+        )
+    with col2:
+        st.markdown(
+            f"""
+            <div style="background-color:#021035;padding:20px;border-radius:10px;text-align:center;margin-bottom:20px;">
+                <h3 style="color:#4baab4">Número de Transações</h3>
+                <p style="font-size:24px;color:white">{num_transactions}</p>
+            </div>
+            """, unsafe_allow_html=True
+        )
+    
+    # Adiciona espaçamento após os cards de métricas
+    st.write("")  
+    st.markdown("### Detalhes das Vendas e Distribuição por Produto")
+    st.write("") 
+
+    # Agrupa por produto e calcula o percentual de cada um
+    product_sales = df.groupby('product')['amount'].sum().reset_index()
+    product_sales['percent'] = (product_sales['amount'] / total_sales) * 100
+    
+    # Organizando a tabela e o gráfico em duas colunas
+    col3, col4 = st.columns(2)
+
+    with col3:
+        st.markdown("#### Tabela de Vendas por Produto")
+        st.dataframe(df)
+
+    with col4:
+        st.markdown("#### Distribuição de Vendas por Produto")
+        # Gráfico de rosca com Plotly
+        fig = px.pie(product_sales, values='amount', names='product', hole=0.4, title="Distribuição de Vendas")
+        st.plotly_chart(fig)
+
+    # Agrupa as vendas por mês e soma os valores
+    sales_over_time = df.groupby(pd.Grouper(key='sale_date', freq='M'))['amount'].sum().reset_index()
+
+    # Criando o gráfico de linha
+    line_fig = px.line(sales_over_time, x='sale_date', y='amount', title='Vendas ao Longo do Tempo')
+    line_fig.update_xaxes(title_text='Data')
+    line_fig.update_yaxes(title_text='Total de Vendas')
+    line_fig.update_xaxes(dtick="M1", tickformat="%b\n%Y")  # Formato para mostrar mês e ano
+    st.plotly_chart(line_fig)
+   
+    st.write("")
