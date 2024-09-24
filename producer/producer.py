2d6493a696d6425d6bbccbc266a713fa1341c650
WillFernandes <wilkerson-carvalho@outlook.com>
2024-09-24 16:35:12 -0300

    Alteração de ordem dos commits

--- producer/producer.py
+++ producer/producer.py
@@ -33,21 +33,21 @@ def generate_random_coordinates():
     
     return latitude, longitude
 
-# Função para buscar dados de venda
+
 def get_sale_data():
-    latitude, longitude = generate_random_coordinates()  # Usa a função atualizada
+    latitude, longitude = generate_random_coordinates()  
     
-    # Adiciona a data e hora atual no formato ISO 8601 periodo máximo de 1 ano
+    
     random_days = random.randint(0, 365)
     sale_date = (datetime.now() - timedelta(days=random_days)).strftime('%Y-%m-%d')
     
     return {
         'sale_id': random.randint(1, 1000),
         'product': random.choice(['Product A', 'Product B', 'Product C']),
-        'amount': round(random.uniform(10.0, 100.0), 2),  # Arredonda o valor para 2 casas decimais
+        'amount': round(random.uniform(10.0, 100.0), 2),  
         'latitude': latitude,
         'longitude': longitude,
-        'sale_date': sale_date  # Adiciona o campo de data
+        'sale_date': sale_date  
     }
 
 # Configuração do Kafka Producer
@@ -57,7 +57,7 @@ producer = KafkaProducer(
     acks='all'
 )
 
-# Loop de envio de mensagens para o Kafka
+
 while True:
     sale_data = get_sale_data()
     producer.send('sales', value=sale_data)
